818b2129b7315febc05246fccb95c463ac48dbc0
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2018-11-11 01:08:34 -0800

    commit

--- main.js
+++ main.js
@@ -1,69 +1,100 @@
-var person = {
-  firstName: "Bill",
-  lastName: "Gates",
-  country: "USA",
-  personId: function(){
-    console.log(`${this.firstName} ${this.lastName} "-- function inside object".`)
+// var person = {
+//   firstName: "Bill",
+//   lastName: "Gates",
+//   country: "USA",
+//   personId: function(){
+//     console.log(`${this.firstName} ${this.lastName} "-- function inside object".`)
+//   }
+// };
+// person.personId();
+// console.log(`${person.firstName} ${person.lastName} is from ${person.country}.`)
+
+// var superman = {
+//   firstName: "Clark",
+//   lastName: "Kent",
+//   superheroName: "Superman",
+//   revealIdentity: function(){
+//     return `${this.firstName} ${this.lastName} is ${this.superheroName}.`
+//   }
+// };
+// console.log(superman.revealIdentity());
+
+// var batman = {
+//   firstName: "Bruce",
+//   lastName: "Wyne",
+//   superheroName: "Batman",
+//   revealIdentity: function(){
+//     return `${this.firstName} ${this.lastName} is ${this.superheroName}.`
+//   }
+// };
+// console.log(batman.revealIdentity());
+
+// //Replace values
+// superman.firstName = 'Duper';
+// superman.lastName =  'Man';
+// console.log(`${superman.revealIdentity()} -- values in superman object are replaced.`);
+
+// //Constructor Object
+
+// var scientist = new Object();
+// scientist.firstName = 'Albert';
+// scientist.lastName = 'Einstein';
+// scientist.discovery = 'Theory of Relativity';
+// scientist.detail = function(){
+//   return `${this.firstName} ${this.lastName} discovered ${this.discovery}.`
+// };
+// console.log(scientist.detail());
+
+// //user-object exmaple
+
+// var user = {
+//   name: "Johny",
+//   calorieTarget: 350,
+//   meals: ["Indian", "Chinese", "Italian"]
+// };
+
+// //now meals will be corrected plus more property-value declared:
+// user.meals = ["breakfast", "lunch", "dinner"];
+// user.mealTaken = "";
+// user.description = "calorie calculation";
+// user.bornOn = "06-10-1996";
+// user.caloriesEaten = 200;
+// user.toEat = function(){
+//   for (var i = 0; i < this.meals.length; i ++){
+//     this.mealTaken = this.meals[i]
+//     console.log(`${this.mealTaken}. Message to ${this.name}: needs to eat ${this.calorieTarget} calories. He ate ${this.caloriesEaten} calories, He was born ${this.bornOn}`)
+//   }
+// };
+
+// user.toEat();
+// ////
+// var Superhero = function () {
+//   console.log('Superhero instance created');
+// };
+
+// // Superhero();
+
+// var clark = new Superhero();
+// //=> "Superhero instance created"
+
+// var bruce = new Superhero();
+// //=> "Superhero instance created"
+
+
+var Superhero = function (firstName, lastName, superheroName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.superheroName = superheroName;
+  this.revealIdentity = function () {
+    console.log(this.firstName + " " + this.lastName + " is " + this.superheroName);
   }
 };
-person.personId();
-console.log(`${person.firstName} ${person.lastName} is from ${person.country}.`)
 
-var superman = {
-  firstName: "Clark",
-  lastName: "Kent",
-  superheroName: "Superman",
-  revealIdentity: function(){
-    return `${this.firstName} ${this.lastName} is ${this.superheroName}.`
-  }
-};
-console.log(superman.revealIdentity());
+var superman = new Superhero("Clark", "Kent", "Superman");
+var batman = new Superhero("Bruce", "Wayne", "Batman");
 
-var batman = {
-  firstName: "Bruce",
-  lastName: "Wyne",
-  superheroName: "Batman",
-  revealIdentity: function(){
-    return `${this.firstName} ${this.lastName} is ${this.superheroName}.`
-  }
-};
-console.log(batman.revealIdentity());
+console.log(`${superman.superheroName} ${superman.lastName}`); //=> "Superman Kent"
 
-//Replace values
-superman.firstName = 'Duper';
-superman.lastName =  'Man';
-console.log(`${superman.revealIdentity()} -- values in superman object are replaced.`);
-
-//Constructor Object
-
-var scientist = new Object(); 
-scientist.firstName = 'Albert';
-scientist.lastName = 'Einstein';
-scientist.discovery = 'Theory of Relativity';
-scientist.detail = function(){
-  return `${this.firstName} ${this.lastName} discovered ${this.discovery}.`
-};
-console.log(scientist.detail());
-
-//user-object exmaple
-
-var user = {
-  name: "Johny",
-  calorieTarget: 350,
-  meals: ["Indian", "Chinese", "Italian"]
-};
-
-//now meals will be corrected plus more property-value declared:
-user.meals = ["breakfast", "lunch", "dinner"];
-user.mealTaken = "";
-user.description = "calorie calculation";
-user.bornOn = "06-10-1996";
-user.caloriesEaten = 200;
-user.toEat = function(){
-  for (var i = 0; i < this.meals.length; i ++){
-    this.mealTaken = this.meals[i]
-    console.log(`${this.mealTaken}. Message to ${this.name}: needs to eat ${this.calorieTarget} calories. He ate ${this.caloriesEaten} calories, He was born ${this.bornOn}`)
-  }
-};
-
-user.toEat();
+console.log(superman.superheroName); //=> "Superman";
+superman.revealIdentity();// => "Clark Kent is Superman";
+batman.revealIdentity();// => "Bruce Wayne is Batman";
